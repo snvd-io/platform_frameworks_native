2ff3cd44c9152854313a02ff36ac814e90777d91
ramindani <ramindani@google.com>
2024-08-27 10:46:26 -0700

    SF: resyncToHardwareVsync after sending a notifyExpectedPresent hint.
    Only send the hint with ScheduleOnTx to target the timeout use case.
    
    BUG: 357801609
    Test: With a trace that we enable the HWVSync on notifyExpectedPresent
    after timeout.
    Flag: EXEMPT bugfix
    
    Change-Id: I1bb6a585e6f40adf2fc9b1d644953460e8f23585

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -4236,6 +4236,8 @@ void SurfaceFlinger::scheduleNotifyExpectedPresentHint(PhysicalDisplayId display
             if (data.hintStatus.compare_exchange_strong(scheduleHintOnTx,
                                                         NotifyExpectedPresentHintStatus::Sent)) {
                 sendHint();
+                constexpr bool kAllowToEnable = true;
+                mScheduler->resyncToHardwareVsync(displayId, kAllowToEnable);
             }
         }));
     }
